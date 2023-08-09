231d071ccd7d64e96859cd9769cf60a7563935f9
L. A. Liggett <liggettla@users.noreply.github.com>
2023-08-09 20:56:38 +0000

    add datepicker to running log

--- running_log/forms.py
+++ running_log/forms.py
@@ -3,6 +3,10 @@ from .models import Run
 
 class RunForm(forms.ModelForm):
     """Form for adding a run."""
+    # create a date field that uses the HTML5 date input datepicker type
+    date = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
     class Meta:
         # The model this form is associated with.
         model = Run
