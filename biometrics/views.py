e10f8dd49f9fa719a32e86cc4943f063a2db4623
L. A. Liggett <liggettla@users.noreply.github.com>
2023-08-11 21:02:06 +0000

    added some more plots

--- biometrics/views.py
+++ biometrics/views.py
@@ -26,14 +26,14 @@ def add_biometrics(request):
     user_biometrics = Biometric.objects.filter(user=request.user).order_by('-date')
 
     # Helper function to create plots
-    def create_plot(attribute, title):
+    def create_plot(attribute, title, biometric='Weight'):
         filtered_biometrics = [biometric for biometric in user_biometrics if getattr(biometric, attribute)]
         weights = [getattr(biometric, attribute) for biometric in filtered_biometrics]
         dates = [(datetime.combine(biometric.date, datetime.min.time()) - datetime(1970, 1, 1)).total_seconds() * 1000 for biometric in filtered_biometrics]
 
-        p = figure(title=title, x_axis_label="Date", y_axis_label="Weight", x_axis_type="datetime", 
+        p = figure(title=title, x_axis_label="Date", y_axis_label=biometric, x_axis_type="datetime", 
                    width=800, height=400, tools="pan,box_zoom,reset,save")
-        p.circle(dates, weights, size=10, color="teal", alpha=0.25, legend_label="Weight")
+        p.circle(dates, weights, size=10, color="teal", alpha=0.25, legend_label=biometric)
 
         slope, intercept, _, _, _ = linregress(dates, weights)
         reg_line = [slope * date + intercept for date in dates]
@@ -50,14 +50,20 @@ def add_biometrics(request):
     # Generate plots
     morning_weight_plot = create_plot("weight_morning", "Morning Weight")
     post_run_weight_plot = create_plot("weight_after_run", "Post-Run Weight")
+    night_weight_plot = create_plot("weight_night", "Night Weight")
+    heart_rate_plot = create_plot("heart_rate", "Heart Rate", "Heart Rate")
 
     # Embed plots into the template
     script_morning, div_morning = components(morning_weight_plot)
     script_postrun, div_postrun = components(post_run_weight_plot)
+    script_night, div_night = components(night_weight_plot)
+    script_hr, div_hr = components(heart_rate_plot)
 
     return render(request, 'biometrics/add_biometrics.html', {
         'form': form,
         'biometrics': user_biometrics,
         'script_morning': script_morning, 'div_morning': div_morning, 
-        'script_postrun': script_postrun, 'div_postrun': div_postrun
+        'script_postrun': script_postrun, 'div_postrun': div_postrun,
+        'script_night': script_night, 'div_night': div_night,
+        'script_hr': script_hr, 'div_hr': div_hr,
     })
