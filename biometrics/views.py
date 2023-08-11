3e6d9ceb73f90bb73d564ca87ff7f92f301db9b8
L. A. Liggett <liggettla@users.noreply.github.com>
2023-08-11 20:31:48 +0000

    generate multiple biometric plots

--- biometrics/views.py
+++ biometrics/views.py
@@ -6,10 +6,8 @@ from .models import Biometric
 # Bokeh imports for plotting
 from bokeh.plotting import figure, show
 from bokeh.embed import components
-from bokeh.models import HoverTool, Span, Label
-from bokeh.layouts import column
+from bokeh.models import HoverTool
 from scipy.stats import linregress
-import numpy as np
 from datetime import datetime
 
 @login_required
@@ -25,45 +23,41 @@ def add_biometrics(request):
         form = BiometricForm()
 
     # Query the database for the user's biometrics
-    # order by date in reverse order (most recent first)
     user_biometrics = Biometric.objects.filter(user=request.user).order_by('-date')
 
-    #########
-    # Plots #
-    #########
+    # Helper function to create plots
+    def create_plot(attribute, title):
+        filtered_biometrics = [biometric for biometric in user_biometrics if getattr(biometric, attribute)]
+        weights = [getattr(biometric, attribute) for biometric in filtered_biometrics]
+        dates = [(datetime.combine(biometric.date, datetime.min.time()) - datetime(1970, 1, 1)).total_seconds() * 1000 for biometric in filtered_biometrics]
 
-    # Filter the user_biometrics to only include entries where weight_morning is not None
-    filtered_biometrics = [biometric for biometric in user_biometrics if biometric.weight_morning]
+        p = figure(title=title, x_axis_label="Date", y_axis_label="Weight", x_axis_type="datetime", 
+                   width=800, height=400, tools="pan,box_zoom,reset,save")
+        p.circle(dates, weights, size=10, color="teal", alpha=0.25, legend_label="Weight")
 
-    # Now, derive weights and dates from this filtered list
-    weights = [biometric.weight_morning for biometric in filtered_biometrics]
-    dates = [(datetime.combine(biometric.date, datetime.min.time()) - datetime(1970, 1, 1)).total_seconds() * 1000 for biometric in filtered_biometrics]
+        slope, intercept, _, _, _ = linregress(dates, weights)
+        reg_line = [slope * date + intercept for date in dates]
+        r = p.line(dates, reg_line, line_color="teal", legend_label=f"Regression (y={slope:.2f}x + {intercept:.2f})")
+        r.muted = True
 
-    # Create the plot
-    p = figure(title="Weight Over Time", x_axis_label="Date", y_axis_label="Weight (kg)", x_axis_type="datetime", 
-           width=800, height=400, tools="pan,box_zoom,reset,save")
-    
-    # Scatter plot
-    p.circle(dates, weights, size=10, color="blue", alpha=0.5, legend_label="Weight")
+        hover = HoverTool()
+        hover.tooltips = [("Date", "@x{%F}"), ("Weight", "@y kg")]
+        hover.formatters = {"@x": "datetime"}
+        p.add_tools(hover)
+        
+        return p
 
-    # Regression line
-    slope, intercept, r_value, p_value, std_err = linregress(dates, weights)
+    # Generate plots
+    morning_weight_plot = create_plot("weight_morning", "Morning Weight")
+    post_run_weight_plot = create_plot("weight_after_run", "Post-Run Weight")
 
-    reg_line = [slope * date + intercept for date in dates]
+    # Embed plots into the template
+    script_morning, div_morning = components(morning_weight_plot)
+    script_postrun, div_postrun = components(post_run_weight_plot)
 
-    r = p.line(dates, reg_line, line_color="red", legend_label=f"Regression (y={slope:.2f}x + {intercept:.2f})")
-    # lighter colors
-    r.muted = True
-    
-    # Add hover tool for better user experience
-    hover = HoverTool()
-    hover.tooltips = [("Date", "@x{%F}"), ("Weight", "@y kg")]
-    hover.formatters = {"@x": "datetime"}
-    p.add_tools(hover)
-
-    # Get components to embed in template
-    script, div = components(p)
-
-    # return render(request, 'biometrics/add_biometrics.html', {'form': form, 'biometrics': user_biometrics})
-
-    return render(request, 'biometrics/add_biometrics.html', {'form': form, 'biometrics': user_biometrics, 'script': script, 'div': div})
+    return render(request, 'biometrics/add_biometrics.html', {
+        'form': form,
+        'biometrics': user_biometrics,
+        'script_morning': script_morning, 'div_morning': div_morning, 
+        'script_postrun': script_postrun, 'div_postrun': div_postrun
+    })
